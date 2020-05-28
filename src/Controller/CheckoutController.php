8b3fea77ddad4fec7c47ee9c666ee22ccafba10f
sbelmahi@yahoo.fr <sbelmahi@yahoo.fr>
2020-05-28 19:00:33 +0200

    Cart commit, panier, checkout and payment

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -1,18 +1,67 @@
-<?php 
-// Set your secret key. Remember to switch to your live secret key in production!
-// See your keys here: https://dashboard.stripe.com/account/apikeys
-\Stripe\Stripe::setApiKey('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+<?php
+namespace App\Controller;
 
-$session = \Stripe\Checkout\Session::create([
-  'payment_method_types' => ['card'],
-  'line_items' => [[
-    'price' => '{{PRICE_ID}}',
+use Stripe\Stripe;
+use Stripe\Checkout\Session;
+use App\Service\Cart\CartService;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CheckoutController extends AbstractController
+{
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * @Route("/checkout")
+     */
+    public function checkout(Request $request)
+    {
+        Stripe::setApiKey('sk_test_iVIe1TqDZbaAebA8IvzYbJoS005YDDkaXE');
+/*
+        $sessionData = [
+            'payment_method_types' => ['card'],
+            'line_items' => [],
+            'success_url' => 'http://localhost:8000/success?session_id={CHECKOUT_SESSION_ID}',
+            'cancel_url' => 'http://localhost:8000/cart_index',
+        ];
+        
+        foreach ($this->session->get('panier') as $item) {
+            $sessionData['line_items'][] = [
+                
+                'description' => 'Votre total',
+                'amount' =>($item['price'] * 100),
+                'currency' => 'eur',
+                'quantity' => 1
+            ];
+            dd($this->session);
+        }
+
+        $session = Session::create($sessionData);
+*/
+
+$sessionData = [
+    'payment_method_types' => ['card'],
+    'line_items' => [[
+    'price' => $_POST['total'],
     'quantity' => 1,
-  ]],
-  'mode' => 'payment',
-  'success_url' => 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-  'cancel_url' => 'https://example.com/cancel',
-]);
+    ]],
+    //'mode' => 'payment',
+    'success_url' => 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+    'cancel_url' => 'https://example.com/cancel',
+];
+        $session = Session::create($sessionData);
+        return $this->render('cart/checkout.html.twig', [
 
-
-?>
+            'sessionId' => $session->id
+        ]);
+    }
+}
